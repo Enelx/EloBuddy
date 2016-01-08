263a6f8e03d5ebf2d31cd71b6429bfd2b229d501
Enelx <sc.wotan@gmx.de>
2016-01-08 12:29:27 +0100

    Auto-Black-Spear
    Kalista Botting Addon

---
+++ Auto-Black-Spear/Auto-Black-Spear/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Drawing;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace Auto_Black_Spear
+{
+    class Program
+    {
+        public static Menu Menu;
+        public static Item BlackSpear;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += On_Loading_Complete;
+        }
+
+        private static void On_Loading_Complete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName != "Kalista") return;
+
+            BlackSpear = new Item(ItemId.The_Black_Spear);
+
+            Menu = MainMenu.AddMenu("Auto-Black-Spear", "AutoBlackSpear");
+            Menu.Add("TrueBlack", new CheckBox("Activate Auto Black Spear"));
+
+            Game.OnUpdate += On_Update;
+        }
+
+        private static void TheBlackSpear()
+        {
+            if (Menu["TrueBlack"].Cast<CheckBox>().CurrentValue && BlackSpear.IsOwned() && Player.Instance.IsInShopRange())
+            {
+                foreach (AIHeroClient ally in EntityManager.Heroes.Allies)
+                {
+                    if (ally != null)
+                    {
+                        BlackSpear.Cast(ally);
+                    }
+                }
+            }
+        }
+
+        private static void On_Update(EventArgs args)
+        {
+            TheBlackSpear();
+        }
+    }
+}
