b3486b680d23bc676151785f21accad6c2ba7a1a
Enelx <sc.wotan@gmx.de>
2016-07-16 16:50:07 +0200

    AAsteering Release

---
+++ AAsteering/AAsteering/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EloBuddy.SDK.Events;
+
+namespace AAsteering
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            Steering.Initialize();
+        }
+    }
+}
---
+++ AAsteering/AAsteering/Steering.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+namespace AAsteering
+{
+    internal class Steering
+    {
+        private static Menu _aamenu;
+
+        public static void Initialize()
+        {
+            Chat.Print("AAsteering loaded, by Enelx");
+
+            _aamenu = MainMenu.AddMenu("AAsteering", "AAsteering");
+            _aamenu.AddLabel("This only works, if your aa range is > target's aa range");
+            _aamenu.Add("UseAA", new CheckBox("Enable AAsteering"));
+
+            Game.OnTick += OnTick;
+        }
+
+        private static void OnTick(EventArgs args)
+        {
+            if (!_aamenu["UseAA"].Cast<CheckBox>().CurrentValue) return;
+
+            foreach (
+                var target in
+                    EntityManager.Heroes.Enemies.Where(
+                        target =>
+                            target.IsValidTarget(Player.Instance.GetAutoAttackRange() + Player.Instance.BoundingRadius) &&
+                            target.IsFacing(Player.Instance) && !target.IsFleeing))
+            {
+                var toTarget = target.ServerPosition - Player.Instance.ServerPosition;
+                var distanceFromEach = toTarget.Length();
+                var amountOfOverlap = Player.Instance.GetAutoAttackRange() + target.GetAutoAttackRange() -
+                                      distanceFromEach;
+
+                if (amountOfOverlap >= 0)
+                {
+                    Orbwalker.MoveTo(toTarget/distanceFromEach*amountOfOverlap);
+                }
+            }
+        }
+    }
+}
